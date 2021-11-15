ed9b7e661509acfb6fd8dd72b78e22ff99b9e309
Julia Tsukerman <julia.tsuk@gmail.com>
2021-11-15 21:57:02 +0300

    model

--- SwiftApp/Model/Model.swift
+++ SwiftApp/Model/Model.swift
@@ -18,32 +18,8 @@ struct User {
             password: DataManager.shared.password,
             name: DataManager.shared.name
         )
-    
-}
-
-enum Questions: String {
-    case easy
-    case middle
-    case hard
-    
-    var definitions: [String] {
-        switch self {
-        case .easy:
-            return DataManager.shared.easyQuestions
-        case .middle:
-            return DataManager.shared.middleQuestions
-        case .hard:
-            return DataManager.shared.hardQuestions
-        }
         
     }
-    
-    func getQuestions() -> String {
-        var question = " "
-        for index in 0..<definitions.count{
-            question = definitions[index]
-        }
-        return question
-    }
-    
 }
+
+
