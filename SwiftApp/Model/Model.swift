a999ca55040ffc06f2d03fe9fc29c420072b45c0
FLX007 <aamiran3007@yandex.ru>
2021-11-15 22:42:38 +0300

    Model enum

--- SwiftApp/Model/Model.swift
+++ SwiftApp/Model/Model.swift
@@ -21,61 +21,29 @@ struct User {
     }
 }
 
-
-struct easyQuestion {
-    let title = DataManager.shared.easyQuestions
-    let answers = DataManager.shared.answers
-    static func getQuestions() -> [easyQuestion] {
-        [
-        ]
-    }
-}
-
-struct middleQuestion {
-    let title = DataManager.shared.middleQuestions
-    let answers = DataManager.shared.answers
-    static func getQuestions() -> [middleQuestion] {
-        [
-        ]
-    }
-}
-
-struct hardQuestion {
-    let title = DataManager.shared.hardQuestions
-    let answers = DataManager.shared.answers
-    static func getQuestions() -> [hardQuestion] {
-        [
-        ]
-    }
-}
-
-
+enum Questions: String {
+    case easy
+    case middle
+    case hard
     
+    var definitions: [String] {
+        switch self {
+        case .easy:
+            return DataManager.shared.easyQuestions
+        case .middle:
+            return DataManager.shared.middleQuestions
+        case .hard:
+            return DataManager.shared.hardQuestions
+        }
+        
+    }
     
+    func getQuestions(case: Questions, index: Int) -> String {
+        var question = " "
+        for index in 0..<definitions.count{
+            question = definitions[index]
+        }
+        return question
+    }
     
-//enum Questions: String {
-//    case easy
-//    case middle
-//    case hard
-//
-//    var definitions: [String] {
-//        switch self {
-//        case .easy:
-//            return DataManager.shared.easyQuestions
-//        case .middle:
-//            return DataManager.shared.middleQuestions
-//        case .hard:
-//            return DataManager.shared.hardQuestions
-//        }
-//
-//    }
-//
-//    func getQuestions() -> String {
-//        var question = " "
-//        for index in 0..<definitions.count{
-//            question = definitions[index]
-//        }
-//        return question
-//    }
-//
-//}
+}
